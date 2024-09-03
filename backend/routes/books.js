d99013887c57be104ae7862cc667531c5878fbb5
Floriane HJ <fjulia.dev@gmail.com>
2024-09-03 11:09:21 +0200

    ajout user + auth

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const bookCtrl = require("../controllers/books");
 
 router.get("/", bookCtrl.getAllBooks);
-router.get("/:id", bookCtrl, getOneBook);
+router.get("/:id", bookCtrl.getOneBook);
 
 module.exports = router;
